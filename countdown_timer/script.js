6d1de062bc4a29fff9a86e5c487be1f176f1f971
hayotbek <hayotbekyoldashev2@gmail.com>
2022-07-07 05:33:06 +0500

    quizapp

--- countdown_timer/script.js
+++ countdown_timer/script.js
@@ -2,7 +2,7 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
-const newYears = "7 July 2022";
+const newYears = "July 7, 2022 03:27:0";
 
 function countdown() {
   const newYearDate = new Date(newYears);
